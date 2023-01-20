04f2f1faa5a27946a9438db2e1c490b08f600261
Brandon Garcia <grandonleebarcia@gmail.com>
2023-01-19 19:54:41 -0800

    add burtons font family

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main className="bg-white px-10" >
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between ">
-             <h1 className="text-xl">developed by Brandon</h1>
+             <h1 className="text-xl font-burtons">developed by Brandon</h1>
              <ul className="flex items-center">
               <li>  
                 <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
