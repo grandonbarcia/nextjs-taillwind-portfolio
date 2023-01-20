2c56e88db6829266f2ffb8aff2b5e2e0444544bb
Brandon Garcia <grandonleebarcia@gmail.com>
2023-01-19 19:51:22 -0800

    add stylings to nav

--- pages/index.js
+++ pages/index.js
@@ -14,12 +14,12 @@ export default function Home() {
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between ">
              <h1 className="text-xl">developed by Brandon</h1>
-             <ul  className="flex items-center">
+             <ul className="flex items-center">
               <li>  
-                <BsFillMoonStarsFill/>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
               </li>
               <li>
-                 <a href="#">Resume</a>
+                 <a className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
               </li> 
              </ul>
           </nav>
