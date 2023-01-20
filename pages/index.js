06f7cd7457b867ca04e1ccc4b89364535917e197
Brandon Garcia <grandonleebarcia@gmail.com>
2023-01-19 20:06:51 -0800

    added content for hero section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import {BsFillMoonStarsFill} from 'react-icons/bs';
+import Head from 'next/head';
+import { BsFillMoonStarsFill } from 'react-icons/bs';
 
 export default function Home() {
   return (
@@ -10,21 +10,34 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-white px-10" >
+      <main className="bg-white px-10">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between ">
-             <h1 className="text-xl font-burtons">developed by Brandon</h1>
-             <ul className="flex items-center">
-              <li>  
-                <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
+            <h1 className="text-xl font-burtons">developed by Brandon</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
               </li>
               <li>
-                 <a className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
-              </li> 
-             </ul>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
           </nav>
+          <div>
+            <h2>Brandon Garcia</h2>
+            <h3> Developer and Designer</h3>
+            <p>
+              Freelancer providing services for programming and design content
+              needs. Join me down below and let's get cracking!
+            </p>
+          </div>
         </section>
       </main>
     </>
-  )
+  );
 }
